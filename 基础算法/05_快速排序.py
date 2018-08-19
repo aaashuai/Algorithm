8950116e181112d9460ed9de29acdd0e14a098e0
一把猫粮 <39478406+Turo233@users.noreply.github.com>
2018-08-19 15:08:49 +0800

    Update 05_快速排序.py

--- 基础算法/05_快速排序.py
+++ 基础算法/05_快速排序.py
@@ -2,16 +2,16 @@ def quickSort(alist, first, last):
 	if first >= last:
 		return
 	mid_value = alist[first]
+	
 	low = first
 	high = last
+	
 	while low < high:
 	    while low < high and alist[high] >= mid_value:
 	        high -= 1
-	    alist[low] = alist[high]
-
 	    while low < high and alist[low] < mid_value:
 	        low += 1
-	    alist[high] = alist[low]
+	    alist[low], alist[high] = alist[high], alist[low]
 
 	alist[high] = mid_value
 
@@ -21,13 +21,11 @@ def quickSort(alist, first, last):
 	# 对low右边进行快排
 	quickSort(alist, low+1, last)
 
-	# quickSort(alist[:low]) 
-	# quickSort(alist[low+1:])
 	return alist
     
 
-
-alist = [132, 3, 4, 2, 55, 66, 5, 44, 4, 1, 2, 5]
-print(alist)
-quickSort(alist, 0, len(alist)-1)
-print(alist)
+if __name__ == "__main__":
+	alist = [132, 3, 4, 2, 55, 66, 5, 44, 4, 1, 2, 5]
+	print(alist)
+	quickSort(alist, 0, len(alist)-1)
+	print(alist)
